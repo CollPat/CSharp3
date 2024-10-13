648ed0d4e986c7f398802af6e823ffb74827d86b
CollPat <166747752+CollPat@users.noreply.github.com>
2024-10-13 16:59:54 +0200

    lekce 3 úprava

--- NewToDoList/src/ToDoList.WebApi/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello world!");
-
-app.MapGet("/NewPath", () => "Zdravím svet!");
-
-app.Run();
