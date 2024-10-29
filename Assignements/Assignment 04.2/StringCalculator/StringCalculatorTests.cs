85150e664641476ffbed32f6a6776932af321deb
CollPat <166747752+CollPat@users.noreply.github.com>
2024-10-29 13:35:01 +0100

    Attempt at A04.2

---
+++ Assignements/Assignment 04.2/StringCalculator/StringCalculatorTests.cs
@@ -0,0 +1,12 @@
+namespace Assignment_04._2;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void Add_EmptyString_ReturnsZero()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("");
+        Assert.Equal(0, result);
+    }
+}
