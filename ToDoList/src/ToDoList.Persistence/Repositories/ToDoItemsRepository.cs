179fb16f1fd911b13bd01fa9c947e1c4d049cd29
ondrej.kasparek <ondrej.kasparek@thermofisher.com>
2024-11-04 18:56:56 +0100

    Feedback Assignment 06.1

--- ToDoList/src/ToDoList.Persistence/Repositories/ToDoItemsRepository.cs
+++ ToDoList/src/ToDoList.Persistence/Repositories/ToDoItemsRepository.cs
@@ -33,6 +33,8 @@ public class ToDoItemsRepository : IRepository<ToDoItem>
         context.SaveChanges();
     }
 
+    //pokud budeme pracovat s ToDoItemsRepository pres interface IRepository<ToDoItem>, tak ten nezna tuto metodu
+    //interface zna pouze public void Delete(T item) - cili public void Delete(ToDoItem item);
     public void Delete(int id)
     {
         var item = context.ToDoItems.Find(id);
@@ -43,6 +45,6 @@ public class ToDoItemsRepository : IRepository<ToDoItem>
         }
     }
 
-    public void Delete(ToDoItem item) => throw new NotImplementedException();
+    public void Delete(ToDoItem item) => throw new NotImplementedException(); //co se stalo? nejaky problem?
 }
 
