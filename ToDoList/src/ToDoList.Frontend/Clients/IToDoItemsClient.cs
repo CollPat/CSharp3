539125905b3e99c2f08dd684af8e328339123b71
CollPat <166747752+CollPat@users.noreply.github.com>
2024-11-20 20:37:45 +0100

    Lekce 9

---
+++ ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
@@ -0,0 +1,9 @@
+using ToDoList.Frontend.Views;
+
+namespace ToDoList.Frontend.Clients
+{
+    public interface IToDoItemsClient
+    {
+        public Task<List<ToDoItemView>> ReadItemsAsync();
+    }
+}
---
+++ ToDoList/src/ToDoList.Frontend/Views/ToDoItemView.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Frontend.Views
+{
+    public class ToDoItemView
+    {
+        public int ToDoItemId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
