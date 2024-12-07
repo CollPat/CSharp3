237ceee2b8ce4432278490e550fe6ca3096b25c8
CollPat <166747752+CollPat@users.noreply.github.com>
2024-12-07 21:08:54 +0100

    Buttons update

--- ToDoList/src/ToDoList.Frontend/Views/ToDoItemView.cs
+++ ToDoList/src/ToDoList.Frontend/Views/ToDoItemView.cs
@@ -9,12 +9,14 @@ namespace ToDoList.Frontend.Views
     public class ToDoItemView
     {
         public int ToDoItemId { get; set; }
-        public required string Name { get; set; }
+
         [Required(ErrorMessage = "Name is mandatory.")]
         [StringLength(50, MinimumLength = 3)]
-        public required string Description { get; set; }
+        public required string Name { get; set; }
+
         [Required(ErrorMessage = "Description is mandatory.")]
         [StringLength(250)]
+        public required string Description { get; set; }
         public bool IsCompleted { get; set; }
 
         public string? Category { get; set; }
